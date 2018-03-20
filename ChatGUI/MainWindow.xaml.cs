4e1b81865450537b99871a55128cdc7e9e588ce9
Kensu Desu <Kensu90@gmail.com>
2018-03-20 22:18:12 +0100

    Add project files.

---
+++ ChatGUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            textWrite.KeyDown += new KeyEventHandler(TW_KeyDown);
+            
+        }
+
+        public void TW_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                textRead.Content += textWrite.Text+Environment.NewLine;
+                textRead.ScrollToBottom();
+                textWrite.Text = null;
+            }
+        }
+    }
+}
